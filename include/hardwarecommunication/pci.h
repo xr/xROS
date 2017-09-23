291796e20c4832f8c749dff20e76f3c50a00971b
liang <guoliang1222@gmail.com>
2017-09-23 20:31:18 +0300

    update PCI

--- include/hardwarecommunication/pci.h
+++ include/hardwarecommunication/pci.h
@@ -1,55 +1,60 @@
+
 #ifndef __XROS__HARDWARECOMMUNICATION__PCI_H
 #define __XROS__HARDWARECOMMUNICATION__PCI_H
 
-#include <common/types.h>
-#include <drivers/driver.h>
-#include <hardwarecommunication/interrupts.h>
 #include <hardwarecommunication/port.h>
+#include <drivers/driver.h>
+#include <common/types.h>
+#include <hardwarecommunication/interrupts.h>
 
-namespace xros {
-  namespace hardwarecommunication {
+namespace xros
+{
+   namespace hardwarecommunication
+   {
 
-    class PCIDeviceDescriptor {
-      public:
-        xros::common::uint32_t portBase;
-        xros::common::uint32_t interrupt;
+       class PeripheralComponentInterconnectDeviceDescriptor
+       {
+       public:
+           xros::common::uint32_t portBase;
+           xros::common::uint32_t interrupt;
 
-        xros::common::uint16_t bus;
-        xros::common::uint16_t device;
-        xros::common::uint16_t function;
+           xros::common::uint16_t bus;
+           xros::common::uint16_t device;
+           xros::common::uint16_t function;
 
-        xros::common::uint16_t vendor_id;
-        xros::common::uint16_t device_id;
+           xros::common::uint16_t vendor_id;
+           xros::common::uint16_t device_id;
 
-        xros::common::uint8_t class_id;
-        xros::common::uint8_t subclass_id;
-        xros::common::uint8_t interface_id;
+           xros::common::uint8_t class_id;
+           xros::common::uint8_t subclass_id;
+           xros::common::uint8_t interface_id;
 
-        xros::common::uint8_t revision;
+           xros::common::uint8_t revision;
 
-        PCIDeviceDescriptor();
-        ~PCIDeviceDescriptor();
-    };
+           PeripheralComponentInterconnectDeviceDescriptor();
+           ~PeripheralComponentInterconnectDeviceDescriptor();
+
+       };
 
 
-    class PCIController {
-      Port32Bit dataPort;
-      Port32Bit commandPort;
+       class PeripheralComponentInterconnectController
+       {
+           Port32Bit dataPort;
+           Port32Bit commandPort;
 
-      public:
-        PCIController();
-        ~PCIController();
+       public:
+           PeripheralComponentInterconnectController();
+           ~PeripheralComponentInterconnectController();
 
-        xros::common::uint32_t Read(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function, xros::common::uint32_t registeroffset);
-        void Write(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function, xros::common::uint32_t registeroffset, xros::common::uint32_t value);
+           xros::common::uint32_t Read(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function, xros::common::uint32_t registeroffset);
+           void Write(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function, xros::common::uint32_t registeroffset, xros::common::uint32_t value);
+           bool DeviceHasFunctions(xros::common::uint16_t bus, xros::common::uint16_t device);
 
-        bool DeviceHasFunctions(xros::common::uint16_t bus, xros::common::uint16_t device);
+           void SelectDrivers(xros::drivers::DriverManager* driverManager);
+           PeripheralComponentInterconnectDeviceDescriptor GetDeviceDescriptor(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function);
+       };
 
-        void SelectDrivers(xros::drivers::DriverManager* driverManager);
-
-        PCIDeviceDescriptor GetDeviceDescriptor(xros::common::uint16_t bus, xros::common::uint16_t device, xros::common::uint16_t function);
-    };
-  }
+   }
 }
 
 #endif
